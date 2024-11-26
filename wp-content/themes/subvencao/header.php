e2729eb660b673bb0c17c44575682010df354bb4
vgiuliani11 <vinicius.giuliani@gmail.com>
2024-11-26 16:56:48 -0300

    Adiciona link para a home no banner

--- wp-content/themes/subvencao/header.php
+++ wp-content/themes/subvencao/header.php
@@ -10,7 +10,9 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <div id="wrapper" class="hfeed">
-        <header id="header" role="banner">
-        </header>
+        <a href="/">
+            <header id="header" role="banner">
+            </header>
+        </a>
         <div id="container">
             <main id="content" role="main">
