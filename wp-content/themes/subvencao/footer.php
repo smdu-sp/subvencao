9da8346833d8deab4eddcebd9ff18f0d82b4a0aa
vgiuliani11 <vinicius.giuliani@gmail.com>
2024-11-26 12:18:53 -0300

    Commit inicial

---
+++ wp-content/themes/subvencao/footer.php
@@ -0,0 +1,8 @@
+            </main>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ wp-content/themes/subvencao/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <div id="wrapper" class="hfeed">
+        <header id="header" role="banner">
+        </header>
+        <div id="container">
+            <main id="content" role="main">
