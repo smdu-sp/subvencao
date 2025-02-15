9f09fcb7b2c7d27317c3b3fbb8f932522566783c
vgiuliani11 <vinicius.giuliani@gmail.com>
2025-02-15 20:37:02 -0300

    Código PHP do accordion

--- wp-content/themes/subvencao/components/accordion/accordion.php
+++ wp-content/themes/subvencao/components/accordion/accordion.php
@@ -1,58 +1,61 @@
 <?php
 
+$arrayChamamentos = Array();
+$menor = 1;
+$maior = 1;
+
+foreach ($subvencionados as $key => $subvencionado) {
+    $chamamento = $subvencionado['chamamento']['processo']['value'];
+    $ordem = $subvencionado['chamamento']['ordem'];
+
+    if ($chamamento > $maior) {
+        $maior = $chamamento;
+    }
+
+    if (! array_key_exists($chamamento, $arrayChamamentos)) {
+        $arrayChamamentos[$chamamento] = array();
+    }
+
+    $arrayChamamentos[$chamamento][$ordem] = $subvencionado;
+}
+
+// Organiza a ordem de mais recente para mais antigo
+for ($i = $maior; $i >= $menor; $i--) {
+    if (array_key_exists($i, $arrayChamamentos)) {
+        // Pega o nome do chamemento do primeiro subvencionado
+        $chamamento = reset($arrayChamamentos[$i])['chamamento']['processo']['label']
 ?>
+        <div class="accordion">
+            <h2>
+                <button
+                    id="accordion-header-<?= $i ?>"
+                    class="titulo"
+                    aria-expanded="false"
+                    aria-controls="accordion-panel-<?= $i ?>"
+                    style="border-color: <?= $cores[$i] ?>">
+                    <span><?= $chamamento ?></span>
+                    <div class="icone" aria-hidden="true">
+                        <?= carregar_svg('chevron-down') ?>
+                    </div>
+                </button>
+            </h2>
+            <section
+                id="`accordion-panel-<?= $i ?>`"
+                aria-labelledby="`accordion-header-<?= $i ?>`"
+                class="conteudo hidden transparente">
+                <?php
+                foreach ($arrayChamamentos[$i] as $key => $chamamento) {
+                    $subvencionado = $chamamento['subvencionado'];
+                    include 'subvencionado.php';
+                }
+                ?>
+            </section>
+        </div>
 
-<div class="accordion">
-    <h2>
-        <button
-            id="`accordion-header-${id}`"
-            class="titulo"
-            aria-expanded="false"
-            aria-controls="`accordion-panel-${id}`"
-            style="border-color: <?= $cores[1] ?>"
-            @click="toggleAccordion()">
-            <span>Chamamento Público nº 03/2024/SMUL</span>
-            <div class="icone" aria-hidden="true">
-                <?= carregar_svg('chevron-down') ?>
-            </div>
-        </button>
-    </h2>
-    <section
-        id="`accordion-panel-${id}`"
-        aria-labelledby="`accordion-header-${id}`"
-        class="conteudo hidden transparente"
-        v-html="conteudo"
-        v-show="accordionAberto">
-        <?php include 'subvencionado.php'; ?>
-    </section>
-</div>
-
-<div class="accordion">
-    <h2>
-        <button
-            type="button"
-            id="`accordion-header-${id}`"
-            class="titulo"
-            aria-expanded="false"
-            aria-controls="`accordion-panel-${id}`"
-            style="border-color: <?= $cores[1] ?>"
-            @click="toggleAccordion()">
-            <span>Chamamento Público nº 03/2024/SMUL</span>
-            <div class="icone" aria-hidden="true">
-                <?= carregar_svg('chevron-down') ?>
-            </div>
-        </button>
-    </h2>
-    <section
-        id="`accordion-panel-${id}`"
-        aria-labelledby="`accordion-header-${id}`"
-        class="conteudo hidden transparente"
-        v-html="conteudo"
-        v-show="accordionAberto">
-        <?php include 'subvencionado.php'; ?>
-    </section>
-</div>
-
+<?php
+    }
+}
+?>
 <script>
     const botoes = document.querySelectorAll('.accordion button')
     for (const botao of botoes) {
@@ -62,13 +65,13 @@
             let timeout = 0
 
             botao.blur()
-            
+
             if (icone.classList.contains('inverter')) {
                 timeout = 300
-            } 
-            
+            }
+
             section.classList.toggle('transparente')
-            
+
             // Adiciona delay no fechamento do accordion
             setTimeout(() => {
                 section.classList.toggle('hidden')
--- wp-content/themes/subvencao/components/accordion/subvencionado.php
+++ wp-content/themes/subvencao/components/accordion/subvencionado.php
@@ -5,83 +5,41 @@
 <div class="container-subvencionado">
     <div class="subvencionado">
         <h3 class="subvencionado-nome">
-            SM01 - Edifício Virgínia SPE S/A
+            <?= $subvencionado['empreendimento']['nome'] ?>
         </h3>
         <div class="subvencionado-info">
             <span>
-                Chamamento Público nº 01/2023/SMUL
+                <?= $chamamento['chamamento']['processo']['label'] ?>
             </span>
             <span>
-                CNPJ: 47.171.025/0001-11
+                CNPJ: <?= $subvencionado['empreendimento']['cnpj'] ?>
             </span>
             <span>
-                Rua Martins Fontes nº 197, 205, 213 e 223 com a Rua Álvaro de Carvalho nº 382 e 386 - Centro, São Paulo/SP.
+                <?= $subvencionado['imovel']['endereco'] ?> - <?= $subvencionado['imovel']['bairro'] ?>, São Paulo/SP.
             </span>
             <span>
-                Responsável pelo imóvel: Fulano de Tal e Coisa
+                Responsável pelo imóvel: <?= $subvencionado['empreendimento']['responsavel'] ?>
             </span>
             <span>
-                <a href="/">Plano Urbanístico</a>
+                <a href="<?= $subvencionado['imovel']['plano_urbanistico'] ?>">Plano Urbanístico</a>
             </span>
         </div>
         <div class="subvencionado-valores">
             <div class="subvencionado-box">
                 <span class="subtitulo">Pontuação</span>
-                <span class="valor">44,23</span>
+                <span class="valor"><?= $subvencionado['subvencao']['pontuacao'] ?></span>
             </div>
             <div class="subvencionado-box">
                 <span class="subtitulo">% Subvenção</span>
-                <span class="valor">11,06%</span>
+                <span class="valor"><?= $subvencionado['subvencao']['pct'] ?>%</span>
             </div>
             <div class="subvencionado-box">
                 <span class="subtitulo">Valor da Subvenção</span>
-                <span class="valor valor-subvencao">R$ 2.647.136,35</span>
+                <span class="valor valor-subvencao">R$ <?= $subvencionado['subvencao']['valor'] ?></span>
             </div>
             <div class="subvencionado-box">
                 <span class="subtitulo">Valor pago</span>
-                <span class="valor valor-pago">R$ xxx.xxx,xx</span>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container-subvencionado">
-    <div class="subvencionado">
-        <h3 class="subvencionado-nome">
-            SM01 - Edifício Virgínia SPE S/A
-        </h3>
-        <div class="subvencionado-info">
-            <span>
-                Chamamento Público nº 01/2023/SMUL
-            </span>
-            <span>
-                CNPJ: 47.171.025/0001-11
-            </span>
-            <span>
-                Rua Martins Fontes nº 197, 205, 213 e 223 com a Rua Álvaro de Carvalho nº 382 e 386 - Centro, São Paulo/SP.
-            </span>
-            <span>
-                Responsável pelo imóvel: Fulano de Tal e Coisa
-            </span>
-            <span>
-                <a href="/">Plano Urbanístico</a>
-            </span>
-        </div>
-        <div class="subvencionado-valores">
-            <div class="subvencionado-box">
-                <span class="subtitulo">Pontuação</span>
-                <span class="valor">44,23</span>
-            </div>
-            <div class="subvencionado-box">
-                <span class="subtitulo">% Subvenção</span>
-                <span class="valor">11,06%</span>
-            </div>
-            <div class="subvencionado-box">
-                <span class="subtitulo">Valor da Subvenção</span>
-                <span class="valor subvencao">R$ 2.647.136,35</span>
-            </div>
-            <div class="subvencionado-box">
-                <span class="subtitulo">Valor pago</span>
-                <span class="valor pago">R$ xxx.xxx,xx</span>
+                <span class="valor valor-pago">R$ <?= $subvencionado['subvencao']['pago'] ?></span>
             </div>
         </div>
     </div>
