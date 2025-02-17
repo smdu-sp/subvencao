ab786ae818ac125e3bd2fae68ecf1593ab88b0ad
vgiuliani11 <vinicius.giuliani@gmail.com>
2025-02-17 16:58:41 -0300

    Resgata as informações para o mapa da página de Transparência

--- wp-content/themes/subvencao/api/rest/empreendimentos.php
+++ wp-content/themes/subvencao/api/rest/empreendimentos.php
@@ -7,17 +7,28 @@ add_action('rest_api_init', function () {
     ]);
 });
 
-function get_empreendimentos_data(WP_REST_Request $request) {
-    global $wpdb;
+function get_empreendimentos_data(WP_REST_Request $request)
+{
+    $transparencia = get_page_by_path('transparencia');
+    $subvencionadosArray = get_field('subvencionados', $transparencia);
+    $response = Array();
 
-    $results = $wpdb->get_results(
-        "SELECT nome, endereco, latitude, longitude, chamamento FROM empreendimentos",
-        ARRAY_A
-    );
+    foreach ($subvencionadosArray as $key => $dados) {
+        $chamamento = $dados['chamamento'];
+        $subvencionado = $dados['subvencionado'];
 
-    if (empty($results)) {
+        array_push($response, Array(
+            'nome' => $subvencionado['empreendimento']['nome'],
+            'endereco' => $subvencionado['imovel']['endereco'],
+            'latitude' => $subvencionado['coordenadas']['latitude'],
+            'longitude' => $subvencionado['coordenadas']['longitude'],
+            'chamamento' => $chamamento['processo']['value']
+        ));
+    }
+
+    if (empty($response)) {
         return new WP_Error('no_data', 'Nenhum empreendimento encontrado.', array('status' => 404));
     }
-    
-    return rest_ensure_response($results);
+
+    return new WP_REST_response($response);
 }
