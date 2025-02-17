0e8cfcf68363ccfa48106df89a042c1d6e71f380
vgiuliani11 <vinicius.giuliani@gmail.com>
2025-02-17 18:26:42 -0300

    Adiciona o responsável aos options do select de Termo de Outorga

--- wp-content/themes/subvencao/api/prestacao-contas/termo-outorga.php
+++ wp-content/themes/subvencao/api/prestacao-contas/termo-outorga.php
@@ -5,11 +5,14 @@ $transparencia = get_page_by_path('transparencia');
 $subvencionados = get_field('subvencionados', $transparencia);
 $termosOutorga = array();
 
-foreach ($subvencionados as $key => $subvencionado) {
-    $termoOutorga = $subvencionado['chamamento']['termo_de_outorga'];
-    $chamamento = $subvencionado['chamamento']['processo']['value'];
-    $ordem = str_pad($subvencionado['chamamento']['ordem'], 3, '0', STR_PAD_LEFT);
-    $id = $chamamento . $ordem;
+foreach ($subvencionados as $key => $dados) {
+    $chamamento = $dados['chamamento'];
+    $subvencionado = $dados['subvencionado'];
+    $responsavel = $subvencionado['empreendimento']['responsavel'];
+    $termoOutorga = $chamamento['termo_de_outorga'];
+    $chamamentoValor = $chamamento['processo']['value'];
+    $ordem = str_pad($chamamento['ordem'], 3, '0', STR_PAD_LEFT);
+    $id = $chamamentoValor . $ordem;
 
-    $termosOutorga[$id] = $termoOutorga;
+    $termosOutorga[$id] = "$termoOutorga - $responsavel";
 }
