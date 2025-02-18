4daa6a3e501307accbc8989870aa577e7508fcc5
vgiuliani11 <vinicius.giuliani@gmail.com>
2025-02-18 14:31:00 -0300

    [Prestação] Remove do select os subvencionados que não possuem termo de outorga, e organiza os itens do select

--- wp-content/themes/subvencao/api/prestacao-contas/termo-outorga.php
+++ wp-content/themes/subvencao/api/prestacao-contas/termo-outorga.php
@@ -8,11 +8,34 @@ $termosOutorga = array();
 foreach ($subvencionados as $key => $dados) {
     $chamamento = $dados['chamamento'];
     $subvencionado = $dados['subvencionado'];
-    $responsavel = $subvencionado['empreendimento']['responsavel'];
     $termoOutorga = $chamamento['termo_de_outorga'];
+
+    if (! $termoOutorga) {
+        continue;
+    }
+
+    $responsavel = $subvencionado['empreendimento']['responsavel'];
     $chamamentoValor = $chamamento['processo']['value'];
     $ordem = str_pad($chamamento['ordem'], 3, '0', STR_PAD_LEFT);
     $id = $chamamentoValor . $ordem;
 
     $termosOutorga[$id] = "$termoOutorga - $responsavel";
 }
+
+uasort($termosOutorga, function($a, $b) {
+    $aAno = substr($a, 3, 4);
+    $bAno = substr($b, 3, 4);
+
+    if ($aAno != $bAno) {
+        return ($aAno < $bAno) ? -1 : 1;
+    }
+
+    $aNum = substr($a, 0, 2);
+    $bNum = substr($b, 0, 2);
+
+    if ($aNum == !$bNum) {
+        return 0;
+    }
+
+    return ($aNum < $bNum) ? -1 : 1;
+});
