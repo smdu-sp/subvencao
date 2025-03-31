4e962631f2da0656ce2fbc8e7848001ac29ec355
vgiuliani11 <vinicius.giuliani@gmail.com>
2025-03-31 17:58:45 -0300

    Correção do template Inscrições

--- wp-content/themes/subvencao/template-inscricoes.php
+++ wp-content/themes/subvencao/template-inscricoes.php
@@ -7,11 +7,7 @@ get_header();
 
 $fields = get_fields();
 $introducao = $fields['introducao'];
-$botao = $fields['botao'];
-
-echo '<pre>';
-print_r($introducao['botao']);
-echo '</pre>';
+$instrucoes = $fields['instrucoes'];
 
 ?>
 
@@ -23,21 +19,21 @@ echo '</pre>';
         <?= $introducao['texto'] ?>
         <?php
         if ($introducao['botao']['url']) { ?>
-        <div class="botao-externo">
-            <a href="<?= $introducao['botao']['url'] ?>">
-                <span>
-                    <?= $introducao['botao']['label'] ?> <?= carregar_svg('external-link') ?>
-                </span>
-            </a>
-        </div>
+            <div class="botao-externo">
+                <a href="<?= $introducao['botao']['url'] ?>">
+                    <span>
+                        <?= $introducao['botao']['label'] ?> <?= carregar_svg('external-link') ?>
+                    </span>
+                </a>
+            </div>
         <?php }
         ?>
     </div>
-    <div class="orientacoes-inscricoes">
-        <h2 class="titulo-inscricoes">
-            <?= $orientacoes['titulo'] ?>
+    <div class="instrucoes-inscricoes">
+        <h2 class="subtitulo-inscricoes">
+            <?= $instrucoes['titulo'] ?>
         </h2>
-        <?= $orientacoes['texto'] ?>
+        <?= $instrucoes['texto'] ?>
     </div>
 </div>
 
